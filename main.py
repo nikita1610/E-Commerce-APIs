15b0e0eac5405b2069c904e43764adcd06f12168
nikita1610 <jainnikita381@gmail.com>
2023-08-22 12:43:57 +0530

    changes in main

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
-from models.users import *
-from models.products import *
-from models.business import *
+from models import *
 
 
 app =FastAPI()
